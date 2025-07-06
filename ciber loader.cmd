1562106d658ba6349e3763ec981a9f1cf99b0d35
system-32-hue <guilhermereisfadigas@gmail.com>
2025-07-06 18:27:04 -0300

    Update ciber loader.cmd

--- ciber loader.cmd
+++ ciber loader.cmd
@@ -5,11 +5,12 @@ echo windows user name / nome de usuario windows
 set /p III=
 echo.
 echo  del /f kill.cmd > update-auto.cmd 
-start https://github.com/system-32-hue/ciber-loader.zip
+start https://github.com/system-32-hue/ciber-loader-updates-
 echo Every time there's an update, always change the name of the old update to "kill.cmd" / toda vez que tiver update sempre troca o nome do antigo update para "kill.cmd" 
 timeout 4
 start update-auto.cmd
 timeout 2
+start https://www.stylemytext.com/
 echo map / mapa
 set /p map=!
 echo.
@@ -18,12 +19,12 @@ echo login or sign up / entra na conta ou criar conta
 set /p input=//
 echo.
 echo welcome back %input%! > keylogI%input%I.txt
-start https://www.stylemytext.com/
 start keylogI%input%I.txt
 echo %map% > %random%_%input%_(key%random%).txt
 timeout 10
 start about:blank
 start https://gifdb.com/images/high/smiling-emoji-bye-bye-fn4s72f5gxm18snr.gif
 exit
+
 
 
